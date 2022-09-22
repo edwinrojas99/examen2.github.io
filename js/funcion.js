3f4a4d7419f3224a4b98b1853886b6d7731b604b
EDWIN ROJAS <103547525+edwinrojas99@users.noreply.github.com>
2022-09-22 19:42:49 -0400

    Add files via upload

---
+++ js/funcion.js
@@ -0,0 +1,14 @@
+function dolares(valNum) {
+    document.getElementById("inputDolares").value=valNum*6.96;
+  }
+  function dolaresR(valNum) {
+    document.getElementById("inputBol").value=valNum*6.96;
+  }
+
+  
+  function bolivianos(valNum2) {
+    document.getElementById("inputDolares2").value=valNum2/6.96;
+  }
+  function bolivianosR(valNum2) {
+    document.getElementById("inputBol2").value=valNum2/6.96;
+  }
